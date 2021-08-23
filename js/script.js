b0cae31cc9d5b8d0442e5ba5b6f65017df139741
CatsPsychologist <mainofficee@gmail.com>
2021-08-23 16:51:27 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,23 +1,48 @@
 'use strict'
 // Метод bind() создаёт новую функцию, которая при вызове устанавливает в качестве
 // контекста выполнения this предоставленное значение.
+//
+// let someObj = {
+//     firstName: 'Ihor',
+//     lastName: 'Cat'
+// }
+//
+// function example(a, b) {
+//     console.log(this, a, b);
+// }
+//
+// const exampleWithContext = example.bind(someObj, 'string1', 'string2');
+//
+// // console.log(exampleWithContext, typeof(exampleWithContext))
+//
+// exampleWithContext();
+// example();
+// //
+// console.log(exampleWithContext === example)
+//
 
 
 let someObj = {
     firstName: 'Ihor',
     lastName: 'Cat'
 }
-
 function example(a, b) {
-    console.log(this, a, b);
+    console.log(a, b);
 }
 
-
-const exampleWithContext = example.bind(someObj, 'string1', 'string2');
-
-// console.log(exampleWithContext, typeof(exampleWithContext))
-
-exampleWithContext();
-example();
+// example(2,3)
+// let a = this;
 //
-console.log(exampleWithContext === example)
+// console.log(a)
+//
+// function thisOne(){
+//     console.log(this)
+// }
+//
+// thisOne();
+//
+// function example(a, b) {
+//     console.log(this, a, b);
+// }
+//
+// console.log(example(2, 3));
